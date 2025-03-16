cfda559253614f55c3de82da011e16b1e4ab8c26
Andrey <andreyi_796@mail.ru>
2025-03-16 13:14:40 +0300

    Change to NavigableMap

--- hw02-collections/src/main/java/homework/CustomerService.java
+++ hw02-collections/src/main/java/homework/CustomerService.java
@@ -8,7 +8,7 @@ public class CustomerService {
     // todo: 3. надо реализовать методы этого класса
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
-    private final SortedMap<Customer, String> customersMap = new TreeMap<>(new Comparator<Customer>() {
+    private final NavigableMap<Customer, String> customersMap = new TreeMap<>(new Comparator<Customer>() {
         @Override
         public int compare(Customer o1, Customer o2) {
             return (int) (o1.getScores() - o2.getScores());
@@ -18,14 +18,11 @@ public class CustomerService {
     });
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        Map.Entry<Customer, String> resultEntry = customersMap.entrySet().stream().findFirst().orElse(null);
-        return copyCustomerEntry(customersMap.entrySet().stream().findFirst().orElse(null));
+        return copyCustomerEntry(customersMap.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        SortedMap<Customer, String> greaterMap = customersMap.tailMap(customer);
-        return copyCustomerEntry(greaterMap.entrySet().stream().filter(entry -> entry.getKey().getId() != customer.getId()).findFirst().orElse(null));
+        return  copyCustomerEntry(customersMap.higherEntry(customer));
 
     }
 
