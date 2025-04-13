ecb54ff09bb369a8b10f2cfc23f92f1aa76ffe9b
Andrey <andreyi_796@mail.ru>
2025-04-13 11:52:18 +0300

    Change to generic method

--- hw05-aop/src/main/java/hw/otus/aop/WorkerDemo.java
+++ hw05-aop/src/main/java/hw/otus/aop/WorkerDemo.java
@@ -1,12 +1,26 @@
 package hw.otus.aop;
 
 import hw.otus.aop.ioc.ProxyIoc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class WorkerDemo {
+    private static final Logger logger = LoggerFactory.getLogger(WorkerDemo.class);
+
     public static void main(String[] args) {
-        WorkerInterface worker = ProxyIoc.createClass();
-        worker.work("123");
-        worker.work2("321", "123");
-        worker.work3(1, "456", "654");
+        WorkerInterface worker = null;
+        try {
+            worker = ProxyIoc.createClass(WorkerInterface.class, WorkerImpl.class);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        if (worker != null) {
+            worker.work("123");
+            worker.work2("321", "123");
+            worker.work3(1, "456", "654");
+        }
     }
 }
--- hw05-aop/src/main/java/hw/otus/aop/ioc/ProxyIoc.java
+++ hw05-aop/src/main/java/hw/otus/aop/ioc/ProxyIoc.java
@@ -1,36 +1,41 @@
 package hw.otus.aop.ioc;
 
-import hw.otus.aop.WorkerImpl;
-import hw.otus.aop.WorkerInterface;
 import hw.otus.aop.annotations.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.lang.reflect.*;
 
+@SuppressWarnings("unchecked")
 public class ProxyIoc {
     private static final Logger logger = LoggerFactory.getLogger(ProxyIoc.class);
 
-    public static WorkerInterface createClass() {
+    public static <T> T createClass(Class<?> interfaceName, Class<?> className) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
-        InvocationHandler handler = new ProxyInvocationHandler(new WorkerImpl());
-        return (WorkerInterface)
-                Proxy.newProxyInstance(ProxyIoc.class.getClassLoader(), new Class<?>[]{WorkerInterface.class}, handler);
+        Constructor<?> constructor = className.getConstructor();
+        InvocationHandler handler = new ProxyInvocationHandler<>((T)constructor.newInstance(new Object[] {}));
+
+        T result;
+        try {
+            result = (T) Proxy.newProxyInstance(ProxyIoc.class.getClassLoader(), new Class<?>[]{interfaceName}, handler);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
-    static class ProxyInvocationHandler implements InvocationHandler {
-        private final WorkerInterface workerClass;
+    static class ProxyInvocationHandler<T> implements InvocationHandler {
+        private final T workerClass;
 
-        ProxyInvocationHandler(WorkerInterface myClass) {
+        ProxyInvocationHandler(T myClass) {
             this.workerClass = myClass;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             if (logMarked(method)) {
-                logger.info("executed method:{}, param:{}", method, args);
+                logger.info("executed method: {}, param: {}", method.getName(), args);
             }
             return method.invoke(workerClass, args);
         }
