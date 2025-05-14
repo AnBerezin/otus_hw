97902eaaf08fe1b9f5bd7d20059d98d23a77002e
Березин Андрей Сергеевич <andreyi_796@mail.ru>
2025-05-14 23:27:25 +0300

    hw08-serialization

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,32 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private String fileName;
+    private final ObjectMapper mapper;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        this.mapper = JsonMapper.builder().build();
+        mapper.registerModule(new JavaTimeModule());
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
         // формирует результирующий json и сохраняет его в файл
+        var file = new File(fileName);
+        try {
+            mapper.writeValue(file, data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -1,15 +1,19 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import ru.otus.model.Measurement;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
+        Map<String, Double> result = new TreeMap<>(Comparator.naturalOrder());
         // группирует выходящий список по name, при этом суммирует поля value
-        return Collections.emptyMap();
+        var dataMap = data.stream().collect(Collectors.groupingBy(Measurement::name, Collectors.summingDouble(Measurement::value)));
+        result.putAll(dataMap);
+        return result;
     }
 }
